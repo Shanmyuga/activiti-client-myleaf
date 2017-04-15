30b0202ace289115d857355d09fb4a6573cff3d9
Rizwan Burhanudeen <ahamath.rizwan@gmail.com>
2017-04-15 00:53:02 -0500

    Simple Form Model

---
+++ src/main/java/com/bpm/activiti/client/model/formdetails/InitialForm.java
@@ -0,0 +1,27 @@
+package com.bpm.activiti.client.model.formdetails;
+
+public class InitialForm {
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	private String name;
+	
+	private String message;
+	
+	
+
+}
